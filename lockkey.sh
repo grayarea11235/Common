4319fa33cea9e90ec25cef079abb905c7d20548a
grayarea11235 <ciaran.dunn@gmail.com>
2021-04-23 19:22:59 +0100

    Added lockkey.sh script

---
+++ lockkey.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#lockkey.sh
+
+sleep .2
+
+case $1 in
+    'num')
+        mask=2
+        key="Num"
+        ;;
+    'caps')
+        mask=1
+        key="Caps"
+        ;;
+esac
+
+value="$(xset q | grep 'LED mask' | awk '{ print $NF }')"
+
+if [ $(( 0x$value & 0x$mask )) == $mask ]
+then
+    output="$key Lock is on"
+else
+    output="$key Lock is off"
+fi
+
+echo $output
+#notify-send "$output"
