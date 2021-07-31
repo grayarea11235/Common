b6a275a878f8a652380c507e05d8b3215f43403b
cpd <ciaran.dunn@gmail.com>
2021-07-31 14:43:05 +0100

    Stuff

--- scripts/backup_github.py
+++ scripts/backup_github.py
@@ -1,33 +1,33 @@
-from urllib.parse import urlparse
-
+from github import Github
 import os
 import pygit2
 
-from github import Github
 
-def get_repo_list():
+def backup_repos():
+    '''
+    This depends on the github toten being set in the environment
+    variable GITHUB_TOKEN.
+    '''
     token = os.environ['GITHUB_TOKEN']
 
-    g = Github(token)
+    git_hub = Github(token)
 
     root_path = '/media/cpd/8067-3833/GithubBackup/'
-    
-    for repo in g.get_user().get_repos():
+    for repo in git_hub.get_user().get_repos():
         print('Repo name is {} and url is {}'.format(repo.name, repo.url))
         dest_path = os.path.join(root_path, repo.name)
 
         print('Cloning to {}'.format(dest_path)) 
-        repoClone = pygit2.clone_repository(repo.git_url, dest_path)
- 
+        repo_clone = pygit2.clone_repository(repo.git_url, dest_path)
+        print(repo_clone)
+
         # We have to use Pygit2
         # https://stackoverflow.com/questions/49458329/create-clone-and-push-to-github-repo-using-pygithub-and-pygit2
 
 
 def main():
-    #repoClone = pygit2.clone_repository(repo.git_url, '/path/to/clone/to')
-    get_repo_list()
-
-    #clone = pygit2.clone_repository(url, '/home/cpd/temp/gittest/Common')
+    ''' Main entry point. '''
+    backup_repos()
 
 
 if __name__ == '__main__':
